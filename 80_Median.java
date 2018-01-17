bbfefa0d8fd84acc6032b1794f22200e68ad578d
linj2n <linj2n@163.com>
2018-01-17 11:42:22 +0800

    update

--- 80_Median.java
+++ 80_Median.java
@@ -7,33 +7,28 @@ public class Solution {
     {
         int temp = nums[i]; nums[i] = nums[j]; nums[j] = temp;
     }
-    public static int partition(int[] nums, int lo, int hi)
-    {
-        int pValue = nums[lo];
-        int i = lo,j = lo + 1;
-        while (j <= hi)
-        {
-            if (nums[j] < pValue)
-            {
+    public static int lomutoPartition(int[] nums, int lo, int hi){
+        int pivotValue = nums[lo];
+        int i = lo, j = lo + 1;
+        while (j <= hi){
+            if (nums[j] < pivotValue)
                 exch(nums,++i,j);
-            }
             ++j;
         }
-        exch(nums,lo,i);
+        exch(nums,i,lo);
         return i;
-    }
+    }    
     public int median(int[] nums) {
         // write your code here
-        int n = nums.length;
-        int m = (n % 2 == 0 ? n/2 - 1 : n / 2);
+        int m = (nums.length - 1) / 2;
         int lo = 0, hi = nums.length - 1;
-        int pivot = partition(nums,lo,hi);
+        int pivot = lomutoPartition(nums,lo,hi);
         while (pivot != m)
         {
             if (pivot > m)
-                pivot = partition(nums,lo,pivot-1);
+                pivot = lomutoPartition(nums,lo,pivot-1);
             else
-                pivot = partition(nums,pivot+1,hi);
+                pivot = lomutoPartition(nums,pivot+1,hi);
         }
         return nums[m];
     }
