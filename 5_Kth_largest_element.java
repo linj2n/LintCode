e3f16056786e9251d4a83553ecf1259c71bc4e60
linj2n <linj2n@163.com>
2018-01-17 14:32:35 +0800

    shorten code

--- 5_Kth_largest_element.java
+++ 5_Kth_largest_element.java
@@ -1,32 +1,18 @@
-import java.util.*;
 class Solution {
     /*
      * @param k : description of k
      * @param nums : array of nums
      * @return: description of return
      */
-    public int kthLargestElement(int k, int[] nums) {
-        // write your code here
-        int KIndex = nums.length - k, lo = 0, hi = nums.length-1;
-        int pivot = lomutoPartition(nums,lo,hi);
-        while (pivot != KIndex)
-        {
-            if (pivot > KIndex)
-                pivot = lomutoPartition(nums,lo,pivot-1);
-            else
-                pivot = lomutoPartition(nums,pivot+1,hi);
-        }
-        return nums[KIndex];
-    }
-    public static void exch(int[] nums, int i, int j){
+    public static void exch(int[] nums, int i, int j)
+    {
         int temp = nums[i]; nums[i] = nums[j]; nums[j] = temp;
     }
     public static int lomutoPartition(int[] nums, int lo, int hi){
         Random r = new Random();
         int p = lo + r.nextInt(hi - lo + 1);
         int pivotValue = nums[p];
-        exch(nums,p,lo);
-        
+        exch(nums,lo,p);
         int i = lo, j = lo + 1;
         while (j <= hi){
             if (nums[j] < pivotValue)
@@ -35,5 +21,19 @@ class Solution {
         }
         exch(nums,i,lo);
         return i;
+    }
+    public int kthLargestElement(int k, int[] nums) {
+        // write your code here
+        int seqKIndex = nums.length - k, lo = 0, hi = nums.length-1;
+        int pivot = lomutoPartition(nums,lo,hi);
+        while (pivot != seqKIndex){
+            if (pivot > seqKIndex){
+                hi = pivot - 1;
+            }else{
+                lo = pivot + 1;
+            }
+            pivot = lomutoPartition(nums,lo,hi);        
+        }
+        return nums[seqKIndex];
     }
 };
--- 80_Median.java
+++ 80_Median.java
@@ -3,10 +3,10 @@ public class Solution {
      * @param : A list of integers
      * @return: An integer denotes the middle number of the array
      */
-    public static void exch(int[] nums, int i, int j)
-    {
+    public static void exch(int[] nums, int i, int j){
         int temp = nums[i]; nums[i] = nums[j]; nums[j] = temp;
     }
+
     public static int lomutoPartition(int[] nums, int lo, int hi){
         int pivotValue = nums[lo];
         int i = lo, j = lo + 1;
@@ -17,18 +17,20 @@ public class Solution {
         }
         exch(nums,i,lo);
         return i;
-    }    
+    }
+    
     public int median(int[] nums) {
         // write your code here
-        int m = (nums.length - 1) / 2;
+        int m = (nums.length-1) / 2;
         int lo = 0, hi = nums.length - 1;
         int pivot = lomutoPartition(nums,lo,hi);
-        while (pivot != m)
-        {
-            if (pivot > m)
-                pivot = lomutoPartition(nums,lo,pivot-1);
-            else
-                pivot = lomutoPartition(nums,pivot+1,hi);
+        while (pivot != m){
+            if (pivot > m){
+                hi = pivot - 1;
+            }else{
+                lo = pivot + 1;
+            }
+            pivot = lomutoPartition(nums,lo,hi);
         }
         return nums[m];
     }
